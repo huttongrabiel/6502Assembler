e7a2cfd04bd598d7647a74bb892866b6a224a615
huttongrabiel <huttonthomas@icloud.com>
2022-04-17 11:49:57 -0700

    Tests: Correctly link multiple test files together into one executable

--- Tests/Translate/TestTranslateInstructionToHexOpcode.cpp
+++ Tests/Translate/TestTranslateInstructionToHexOpcode.cpp
@@ -1,9 +1,9 @@
 #include <catch2/catch_test_macros.hpp>
 #include <Translate.h>
 
-Translate translate;
-
 TEST_CASE( "Translate Instruction to Its OpCode in Hex" ) {
+    Translate translate;
+
     REQUIRE( translate.translate_instruction_to_hex_opcode("LDA $XX X") == 0xB5 );
     REQUIRE( translate.translate_instruction_to_hex_opcode("INX") == 0xE8 );
 }
