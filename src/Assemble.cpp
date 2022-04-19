d92ebba1eb85a82b0b72266d98dd6a929c25fe83
huttongrabiel <huttonthomas@icloud.com>
2022-04-18 18:56:38 -0700

    Assemble: Add some more information to std::cout not important
    Assemble is more of a testing ground right now. Soon it will handle
    outputting the binary values to an executable but for now, eh.

--- src/Assemble.cpp
+++ src/Assemble.cpp
@@ -25,15 +25,24 @@ int main(int argc, char* argv[]) {
 
     while (std::getline(source_code, line)) {
         Tokenizer tokenizer;
+       
         auto trimmed_line = tokenizer.remove_whitespace(line);
         trimmed_line = tokenizer.remove_comments(trimmed_line);
-        std::vector<std::string> tokenized_line = tokenizer.tokenize_line(trimmed_line);
+        std::vector<std::string> const tokenized_line = tokenizer.tokenize_line(trimmed_line);
         
+        int const oper_index = tokenizer.index_of_oper_in_tokenized_line(tokenized_line);
+        std::string const oper = tokenizer.oper(tokenized_line[oper_index]);
+
         Translate translate;
+    
+        // These return -1 when there is no oper found
+        int const oper_low_byte = translate.oper_low_byte(oper);
+        int const oper_high_byte = translate.oper_high_byte(oper);
+
         auto const standardized_instruction = translate.standardize_instruction(tokenized_line);
-        auto const translated_instruction = translate.translate_instruction_to_hex_opcode(standardized_instruction);
-        
-        std::cout << standardized_instruction << " : " << translated_instruction << std::endl;
+        auto const instruction_hex_opcode = translate.translate_instruction_to_hex_opcode(standardized_instruction);
+
+        std::cout << standardized_instruction << " : " << instruction_hex_opcode << " " << oper_low_byte << " " << oper_high_byte << std::endl;
     }
 
     source_code.close();
