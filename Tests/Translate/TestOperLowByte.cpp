01d4d7a05323b91744168fe9df3164e6bb09a020
huttongrabiel <huttonthomas@icloud.com>
2022-04-18 19:14:16 -0700

    TranslateTests: Change test opers to not include $ or #
    Those are trimmed off before we pass to oper_*_byte so having them in
    the tests will cause the tests to fail because the functions are not
    designed to handle them.

--- Tests/Translate/TestOperLowByte.cpp
+++ Tests/Translate/TestOperLowByte.cpp
@@ -4,8 +4,8 @@
 TEST_CASE( "16 Bit Little Endian Least Significant Byte Extraction" ) {
     Translate translate;
 
-    REQUIRE( translate.oper_low_byte("$8347") == 47 );
-    REQUIRE( translate.oper_low_byte("#$7263") == 63 );
-    REQUIRE( translate.oper_low_byte("$38") == 0 );
-    REQUIRE( translate.oper_low_byte("#$98") == 0 );
+    REQUIRE( translate.oper_low_byte("8347") == 47 );
+    REQUIRE( translate.oper_low_byte("7263") == 63 );
+    REQUIRE( translate.oper_low_byte("38") == 0 );
+    REQUIRE( translate.oper_low_byte("98") == 0 );
 }
