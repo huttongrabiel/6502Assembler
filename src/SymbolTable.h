25e84c674379f2020761e29ca44ba79d565b1d56
huttongrabiel <huttonthomas@icloud.com>
2022-05-16 11:27:30 -0700

    SymbolTable: Rewrite, read description
    Make functions static as we only deal with static variables in the
    SymbolTable class. Change the function calls in Assemble to use static
    method calls. Mark print_symbol_table as maybe_unused due it being more
    of a debugging function that is in fact maybe unused. Change
    constructor and destructor to default and don't call them in cpp file.
    Rename line to buffer because it is a buffer. Make most parameters
    const/const&.

--- src/SymbolTable.h
+++ src/SymbolTable.h
@@ -1,6 +1,6 @@
-//
-// Created by whuty on 4/9/22.
-//
+/*
+ * Copyright 2022, Hutton Grabiel
+ */
 
 #ifndef SYSTEMMONITOR_SYMBOLTABLE_H
 #define SYSTEMMONITOR_SYMBOLTABLE_H
@@ -8,22 +8,18 @@
 #include <iostream>
 #include <map>
 #include <fstream>
-#include <Tokenizer.h>
-#include <Translate.h>
 
 class SymbolTable {
 public:
     typedef std::map<const std::string, const int> SymbolTableMap;
-    typedef std::string SymbolName;
-    typedef int ProgramLine;
 
-    SymbolTable();
-    ~SymbolTable();
+    SymbolTable() = default;
+    ~SymbolTable() = default;
 
-    void insert(SymbolName, ProgramLine);
-    void fill_symbol_table(std::ifstream&);
-    void print_symbol_table();
-    bool is_label(std::string);
+    static void insert(std::string const&, int);
+    static void fill_symbol_table(std::ifstream&);
+    [[maybe_unused]] static void print_symbol_table();
+    static bool is_label(std::string const&);
 
     static SymbolTableMap symbol_table;
     static int m_program_counter;
