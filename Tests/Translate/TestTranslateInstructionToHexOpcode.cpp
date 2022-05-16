02d1f42b1aedf354861660e8ee25a835bd61516e
huttongrabiel <huttonthomas@icloud.com>
2022-05-15 21:42:40 -0700

    TranslateTests: Pass dummy parameter
    Pass 0 where it usually will take program_counter.

--- Tests/Translate/TestTranslateInstructionToHexOpcode.cpp
+++ Tests/Translate/TestTranslateInstructionToHexOpcode.cpp
@@ -5,6 +5,7 @@
 TEST_CASE( "Translate Instruction to Its OpCode in Hex" ) {
     Translate translate;
 
-    REQUIRE( translate.translate_instruction_to_hex_opcode("LDA $XX X") == 0xB5 );
-    REQUIRE( translate.translate_instruction_to_hex_opcode("INX") == 0xE8 );
+    // Second parameter is just a placeholder in testing. It is the program counter typically
+    REQUIRE( translate.translate_instruction_to_hex_opcode("LDA $XX X", 0) == 0xB5 );
+    REQUIRE( translate.translate_instruction_to_hex_opcode("INX", 0) == 0xE8 );
 }
