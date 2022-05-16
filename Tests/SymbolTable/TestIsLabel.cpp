48b18db1d604bcb5feec2092600fc891126babb0
huttongrabiel <huttonthomas@icloud.com>
2022-05-16 13:32:47 -0700

    Tests: Remove class instances + rename one test
    Rename TestIndexOfOperInTokenizedLine to
    TestOperStartIndexInTokenizedLine. Call class functions statically.
    All tests pass.

--- Tests/SymbolTable/TestIsLabel.cpp
+++ Tests/SymbolTable/TestIsLabel.cpp
@@ -2,10 +2,9 @@
 #include <SymbolTable.h>
 
 TEST_CASE( "Check if Instruction Is Label" ) {
-    SymbolTable symbolTable;
-    REQUIRE( symbolTable.is_label("LOOP") == false );
-    REQUIRE( symbolTable.is_label("LOOP:") == true );
-    REQUIRE( symbolTable.is_label("BNE") == false );
-    REQUIRE( symbolTable.is_label("LDA #$4853, X") == false );
-    REQUIRE( symbolTable.is_label("WHILE:") == true );
+    REQUIRE( SymbolTable::is_label("LOOP") == false );
+    REQUIRE( SymbolTable::is_label("LOOP:") == true );
+    REQUIRE( SymbolTable::is_label("BNE") == false );
+    REQUIRE( SymbolTable::is_label("LDA #$4853, X") == false );
+    REQUIRE( SymbolTable::is_label("WHILE:") == true );
 }
--- Tests/Translate/TestTranslationHelperIsBranchInstruction.cpp
+++ Tests/Translate/TestTranslationHelperIsBranchInstruction.cpp
@@ -2,18 +2,16 @@
 #include <Translate.h>
 
 TEST_CASE( " Instruction is branch instruction or not " ) {
-    TranslationHelpers translationHelpers;
+    REQUIRE( TranslationHelpers::is_branch_instruction("BPL") == true );
+    REQUIRE( TranslationHelpers::is_branch_instruction("BMI") == true );
+    REQUIRE( TranslationHelpers::is_branch_instruction("BVC") == true );
+    REQUIRE( TranslationHelpers::is_branch_instruction("BVS") == true );
+    REQUIRE( TranslationHelpers::is_branch_instruction("BCC") == true );
+    REQUIRE( TranslationHelpers::is_branch_instruction("BCS") == true );
+    REQUIRE( TranslationHelpers::is_branch_instruction("BNE") == true );
+    REQUIRE( TranslationHelpers::is_branch_instruction("BEQ") == true );
 
-    REQUIRE( translationHelpers.is_branch_instruction("BPL") == true );
-    REQUIRE( translationHelpers.is_branch_instruction("BMI") == true );
-    REQUIRE( translationHelpers.is_branch_instruction("BVC") == true );
-    REQUIRE( translationHelpers.is_branch_instruction("BVS") == true );
-    REQUIRE( translationHelpers.is_branch_instruction("BCC") == true );
-    REQUIRE( translationHelpers.is_branch_instruction("BCS") == true );
-    REQUIRE( translationHelpers.is_branch_instruction("BNE") == true );
-    REQUIRE( translationHelpers.is_branch_instruction("BEQ") == true );
-
-    REQUIRE( translationHelpers.is_branch_instruction("INX") == false );
-    REQUIRE( translationHelpers.is_branch_instruction("BRK") == false );
-    REQUIRE( translationHelpers.is_branch_instruction("STA #$54, X") == false );
+    REQUIRE( TranslationHelpers::is_branch_instruction("INX") == false );
+    REQUIRE( TranslationHelpers::is_branch_instruction("BRK") == false );
+    REQUIRE( TranslationHelpers::is_branch_instruction("STA #$54, X") == false );
 }
